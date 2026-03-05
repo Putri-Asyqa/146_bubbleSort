56d10209dbfdd0b82b067564c24e11732d34275e
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-03-05 10:57:50 +0700

    output to screen

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -9,6 +9,20 @@ void input() {       // procedur untuk input
      while (true) {
         cout << "Masukkan banyaknya elemen pada array : "; 
         cin >> n;       // Input dari pengguna
+        if (n <= 20)    // Jika n kurang dari atau sama dengan 20
+            break;      // Keluar dari loop
+        else {          //  Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+        }      
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++) {      // Looping dengan i dimulai dari 0
+        cout << "Data ke-" << (i+i) << ": ";   // Output ke layar
+
     }
    
 }
