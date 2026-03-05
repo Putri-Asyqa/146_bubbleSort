4e8367a952e351a09e509a40cc08dcafb1839e6e
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-03-05 10:43:42 +0700

    make procedure for input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,3 +4,7 @@ using namespace std;
 
 int arr[20]; // Deklarasi variable global array a dengan ukuran 20
 int n; //Deklarasi variabel global n untuk menyimpan banyaknya elemen
+
+void input() {       // procedur untuk input
+   
+}
